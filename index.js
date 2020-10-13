40d2e96ac39786d095ae23930958f22ff32ce85b
Kuldeep K Srivastava <kk23907.me@gmail.com>
2020-10-14 01:46:07 +0530

    to deploy

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
-const accountSid = 'AC9d9426c8601e0025e2d46bad36816ada';
-const authToken = '3f21e76a763fa391d701c869b371c81f';
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 const sendAMessage = (data, callback) => {
@@ -10,7 +10,7 @@ const sendAMessage = (data, callback) => {
     client.messages
     .create({
        body: "The amount  " + data.amount + " is going to be " + data.action + "  from the address " + data.member + " Please provide this OTP to proceed " + data.otp,
-       from: '+12055962414',
+       from: process.env.MY_NUMBER,
        to: data.to
      })
     .then(message => {
